2a318b55974c779cb9bf6ed38ead0efaf8440724
azureitstepkharkov <azure.itstep.kharkov@gmail.com>
2019-09-10 20:10:59 +0300

    Изменена архитектура приложения: введены классы по принципам SOLID обоснование паттерна Singleton и механизма делегатов в C#

--- Ivan Petrov/Pong/Pong/pch.h
+++ Ivan Petrov/Pong/Pong/pch.h
@@ -8,7 +8,10 @@
 
 #ifndef PCH_H
 #define PCH_H
-
-// TODO: add headers that you want to pre-compile here
-
+// a few keycodes from 
+// http://msdn.microsoft.com/en-us/library/windows/desktop/dd375731%28v=vs.85%29.aspx
+#define VK_W 0x57
+#define VK_S 0x53
 #endif //PCH_H
+
+//
