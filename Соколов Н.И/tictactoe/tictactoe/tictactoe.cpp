8aae5dce5442609265711a85f5890d4c7305b1d4
Mr.ananimus <rollo2133@gmail.com>
2019-09-10 20:42:58 +0300

    11

---
+++ Соколов Н.И/tictactoe/tictactoe/tictactoe.cpp
@@ -0,0 +1,28 @@
+#include "BoardMatrix.h"
+#include "pch.h"
+#include"GameBoard.h"
+#include "GameController.h"
+
+int main()
+{
+    std::cout << "Hello from tictatoe game"<< endl;
+	GameController* tictactoegame = new GameController();
+	bool haveViner = false;
+	
+
+	do
+	{
+	
+	tictactoegame->Game(); // спрашиваю куда ставить Х или О
+	haveViner = tictactoegame->haveViner();// ищу линию Х или О 
+	system("cls");
+	tictactoegame->printresult();// вывожу текущую игровую ситуацию
+	
+	
+	} while (!haveViner); 
+
+	
+	//delete tictactoegame;
+}
+
+
