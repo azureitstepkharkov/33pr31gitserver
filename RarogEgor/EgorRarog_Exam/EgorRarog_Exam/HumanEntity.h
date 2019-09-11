0bcc60d616ca5bb55287c01685c2240e78e35e8f
itstepkharkov <egorrarog@gmail.com>
2019-09-11 20:51:57 +0300

    Екзамен

---
+++ RarogEgor/EgorRarog_Exam/EgorRarog_Exam/HumanEntity.h
@@ -0,0 +1,33 @@
+#pragma once
+#include"AdressInfo.h"
+#include"AnimalEntity.h"
+#include<string>
+#include<iostream>
+using namespace std;
+class HumanEntity : public AnimalEntity
+{
+protected:
+	string surname;
+	string name;
+	HumanEntity() { cout << "call HumanEntity()" << endl; };
+	HumanEntity(const HumanEntity& obj) { cout << "const HumanEntity& obj" << endl; };
+	~HumanEntity() {};
+public:
+	HumanEntity(string name, string surname) {
+		this->name = name;
+		this->surname = surname;
+	}
+	void setTel_num(string info) { this->name = info; }
+	void setAdr(string surname) { this->surname = surname; }
+	string getName() { return name; }
+	string getSurname() { return surname; }
+	friend ostream& operator << (ostream& out_data, HumanEntity& h)
+	{
+		out_data << "Name is " << h.name << endl;
+		out_data << "Surname is" << h.surname << endl;
+
+		return out_data;
+	};
+
+	friend class Person;
+};
