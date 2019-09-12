0f760ed290e3c15e67618a587ea8dcf145400ddb
koctik2110 <koctik2110@gmail.com>
2019-09-12 18:13:12 +0300

    Exam TicTacToe, MyAdress

---
+++ Exam/Kharchenko.Konstantin/tictactoe/tictactoe/GameBoard.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class GameBoard
+{
+private:
+	const int N=3;
+public:
+	GameBoard()
+	{
+		/*for (int i(0); i < N; i++)
+		{
+			for (int j(0); j < N; j++)
+				cout << "_" << " ";
+			cout << endl;
+		}*/																										   
+	}
+	GameBoard(int ** board)
+	{
+		DrawGameBoard(board);
+	}
+	void DrawGameBoard(int ** board)
+	{
+		for (int i(0); i < N; i++)
+		{
+			for (int j(0); j < N; j++)
+			{
+				if (board[i][j] == 2)
+				{
+					cout << "_" << " ";
+				}
+				else if(board[i][j] == 1)
+					cout << "X ";
+				else if (board[i][j] == 0)
+					cout  << "O " ;
+
+			}
+			cout << endl;
+			
+		}
+		
+	}
+	virtual ~GameBoard() {};
+	int getN()
+	{
+		return this->N;
+	}
+};
+
